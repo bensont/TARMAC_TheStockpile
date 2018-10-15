0ea2472d0eb83f202ad1d7310baa1791257b24b5
Alex <akennedy4155@gmail.com>
2018-10-15 14:52:35 -0700

    0.0.5, next step is turning data into CSV and JSON

--- WebScraper/main_scrape_v2.py
+++ WebScraper/main_scrape_v2.py
@@ -12,19 +12,10 @@ def remove_dupes(product_list):
 # Globals
 product_object_list = []
 full_individual_product_soups = []
-
-main_url = "https://www.ikea.com"
-# URLs for various pages as strings
-couch_url = "https://www.ikea.com/us/en/catalog/categories/departments/living_room/39130/"
-sleeper_url = "https://www.ikea.com/us/en/catalog/categories/departments/living_room/10663/"
-# all_urls = []
-# all_urls.append(couch_url)
-# all_urls.append(sleeper_url)
 all_urls = url_tools.build_url_list_from_txt_file(sys.argv[1])
-# print(url_tools.build_url_list_from_txt_file(sys.argv[1]))
 # turn all of the URLs into soup and elements of those pages into soup
 for a_url in all_urls:
     full_individual_product_soups += url_tools.build_url_soup_list(a_url, 10)
-
+#remove duplicates and print the len of the product list
 full_individual_product_soups = remove_dupes(full_individual_product_soups)
 print(len(full_individual_product_soups))
