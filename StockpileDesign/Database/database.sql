2780813bee5b3bef7430e980af7089bbf6ba0d41
bensont <36897397+bensont@users.noreply.github.com>
2018-10-30 23:23:33 -0600

    Fixed syntax errors and added product names

--- StockpileDesign/Database/database.sql
+++ StockpileDesign/Database/database.sql
@@ -36,17 +36,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Users` (
 ENGINE = InnoDB;
 
 INSERT INTO Users (loginID, password, PermissionsLevel, StreetAddress, City, State, ZipCode, DeliveryInstructions, email)
-INSERT INTO Users ("admin","password","admin","54 Red Street","New York City","New York", 10027,"Drop on porch", "spiderman@marvelheroes.org", ?)
-
-INSERT INTO Users ("Bruce Banner","hulk","customer","123 Green Street","Dayton","Ohio", 45377,"Drop on porch", "hulk@marvelheroes.org")
-INSERT INTO Users ("Anthony Stark","ironman","customer","456 Red Lane","Long Island","New York", 11101,"Signature required", "ironman@marvelheroes.org")
-INSERT INTO Users ("Steve Rogers","captainamerica","customer","159 Blue Drive","New York City","New York", 10087,"Call before delivery", "captainamerica@marvelheroes.org")
-INSERT INTO Users ("Stephen Strange","dorctorstrange","customer","753 Purple Street","Philadelphia","Pennsylvania", 19135,"Signature required", "doctorstrange@marvelheroes.org")
-INSERT INTO Users ("Peter Parker","spiderman","customer","54 Red Street","New York City","New York", 10027,"Drop on porch", "spiderman@marvelheroes.org")
-INSERT INTO Users ("Scott Lang","antman","customer","4876 Anthony Drive","Coral Gables","Florida", 33134,"Call before delivery", "antman@marvelheroes.org")
-INSERT INTO Users ("Peter Quill","starlord","customer","6698 Space Lane","St. Charles","Missouri", 63302,"Call before delivery, I may be way, way out.", "starlord@marvelheroes.org")
-INSERT INTO Users ("Wade Wilson","deadpool","customer","9855 Undead Stree","New York City","New York", 10115,"Signature required", "deadpool@marvelheroes.org")
-INSERT INTO Users ("Logan Wilson","wolverine","customer","1407 Graymalkin Lane","Salem Center","New York", 10560,"Drop on porch", "wolverine@marvelheroes.org")
+VALUES ("admin","password","admin","54 Red Street","New York City","New York", 10027,"Drop on porch", "spiderman@marvelheroes.org"),
+VALUES ("Bruce Banner","hulk","customer","123 Green Street","Dayton","Ohio", 45377,"Drop on porch", "hulk@marvelheroes.org"),
+VALUES ("Anthony Stark","ironman","customer","456 Red Lane","Long Island","New York", 11101,"Signature required", "ironman@marvelheroes.org"),
+VALUES ("Steve Rogers","captainamerica","customer","159 Blue Drive","New York City","New York", 10087,"Call before delivery", "captainamerica@marvelheroes.org"),
+VALUES ("Stephen Strange","dorctorstrange","customer","753 Purple Street","Philadelphia","Pennsylvania", 19135,"Signature required", "doctorstrange@marvelheroes.org"),
+VALUES ("Peter Parker","spiderman","customer","54 Red Street","New York City","New York", 10027,"Drop on porch", "spiderman@marvelheroes.org"),
+VALUES ("Scott Lang","antman","customer","4876 Anthony Drive","Coral Gables","Florida", 33134,"Call before delivery", "antman@marvelheroes.org"),
+VALUES ("Peter Quill","starlord","customer","6698 Space Lane","St. Charles","Missouri", 63302,"Call before delivery, I may be way, way out.", "starlord@marvelheroes.org"),
+VALUES ("Wade Wilson","deadpool","customer","9855 Undead Stree","New York City","New York", 10115,"Signature required", "deadpool@marvelheroes.org"),
+VALUES ("Logan Wilson","wolverine","customer","1407 Graymalkin Lane","Salem Center","New York", 10560,"Drop on porch", "wolverine@marvelheroes.org");
 
 
 -- -----------------------------------------------------
@@ -71,33 +70,33 @@ ENGINE = InnoDB;
 
 INSERT INTO Products (Name, Brand, Type, Price, Material, Image1, Image2, Image3, Description, Dimensions, Stock)
 
-INSERT INTO Products ("Good Wood", "Chair", 169.00, "Oak", chair2a.jpg, chair2b.jpg, chair2c.jpg, "A beautifully crafted chair.", "25 x 29.5 x 31 inches", 5)
-INSERT INTO Products ("Custom Crafts", "Chair", 199.99, "Oak/Metal", chair3a.jpg, chair3b.jpg, chair3c.jpg, "A chair superior to any bar stool.", "25.5 x 30 x 30.5 inches", 5)
-INSERT INTO Products ("Encono-ture", "Chair", 69.00, "Pine", chair1a.jpg, chair1b.jpg, chair1c.jpg, "A basic, but functional chiar", "26 x 30 x 31 inches", 5)
-INSERT INTO Products ("Good Wood", "Chair", 249.00, "Oak/Metal", chair4a.jpg, chair4b.jpg, chair4c.jpg, "A chair that puts comfort in your behind.", "27 x 32 x 31.5 inches", 5)
-INSERT INTO Products ("Custom Crafts", "Chair", 289.00, "Oak", chair5a.jpg, chair5b.jpg, chair5c.jpg, "A classic rocking chair. Quality.", "27.5 x 31 x 31.5 inches", 5)
+VALUES ("Classic","Good Wood", "Chair", 169.00, "Oak", chair2a.jpg, chair2b.jpg, chair2c.jpg, "A beautifully crafted chair.", "25 x 29.5 x 31 inches", 5),
+VALUES ("Classy Classic","Custom Crafts", "Chair", 199.99, "Oak/Metal", chair3a.jpg, chair3b.jpg, chair3c.jpg, "A chair superior to any bar stool.", "25.5 x 30 x 30.5 inches", 5),
+VALUES ("Royal Bar","Encono-ture", "Chair", 69.00, "Pine", chair1a.jpg, chair1b.jpg, chair1c.jpg, "A basic, but functional chiar", "26 x 30 x 31 inches", 5),
+VALUES ("Cushoned Presidential","Good Wood", "Chair", 249.00, "Oak/Metal", chair4a.jpg, chair4b.jpg, chair4c.jpg, "A chair that puts comfort in your behind.", "27 x 32 x 31.5 inches", 5),
+VALUES ("Porch Rocker","Custom Crafts", "Chair", 289.00, "Oak", chair5a.jpg, chair5b.jpg, chair5c.jpg, "A classic rocking chair. Quality.", "27.5 x 31 x 31.5 inches", 5),
 
-INSERT INTO Products ("Good Wood", "dresser", 369.00, "Oak", dresser1a.jpg, dresser1b.jpg, dresser1c.jpg, "A beautifully crafted dresser. Mirror included.", "25 x 29.5 x 31 inches", 5)
-INSERT INTO Products ("Custom Crafts", "dresser", 299.99, "Maple", dresser2a.jpg, dresser2b.jpg, dresser2c.jpg, "Classic, beautifully designed with uniquely patterned wood.", "25.5 x 30 x 30.5 inches", 5)
-INSERT INTO Products ("Encono-ture", "dresser", 469.00, "Oak", dresser3a.jpg, dresser3b.jpg, dresser3c.jpg, "A large dresser with lots of space.", "26 x 30 x 31 inches", 5)
-INSERT INTO Products ("Good Wood", "dresser", 249.00, "Oak/Metal", dresser4a.jpg, dresser4b.jpg, dresser4c.jpg, "A smaller dresser. Very useful as a nightstand.", "27 x 32 x 31.5 inches", 5)
+VALUES ("Mirrored Princess","Good Wood", "dresser", 369.00, "Oak", dresser1a.jpg, dresser1b.jpg, dresser1c.jpg, "A beautifully crafted dresser. Mirror included.", "25 x 29.5 x 31 inches", 5),
+VALUES ("Vintage","Custom Crafts", "dresser", 299.99, "Maple", dresser2a.jpg, dresser2b.jpg, dresser2c.jpg, "Classic, beautifully designed with uniquely patterned wood.", "25.5 x 30 x 30.5 inches", 5),
+VALUES ("London Classic","Encono-ture", "dresser", 469.00, "Oak", dresser3a.jpg, dresser3b.jpg, dresser3c.jpg, "A large dresser with lots of space.", "26 x 30 x 31 inches", 5),
+VALUES ("London Bedside","Good Wood", "dresser", 249.00, "Oak/Metal", dresser4a.jpg, dresser4b.jpg, dresser4c.jpg, "A smaller dresser. Very useful as a nightstand.", "27 x 32 x 31.5 inches", 5),
 
-INSERT INTO Products ("White Glow", "lighting", 229.00, "Glass/Metal", light1a.jpg, light1b.jpg, light1c.jpg, "Triple the beauty!", "25 x 29.5 x 31 inches", 5)
-INSERT INTO Products ("Custom Crafts", "lighting", 199.99, "Glass/Metal", light2a.jpg, light2b.jpg, light2c.jpg, "Six overhead lights.", "25.5 x 30 x 30.5 inches", 5)
-INSERT INTO Products ("Encono-ture", "lighting", 179.00, "Glass/Metal", light3a.jpg, light3b.jpg, light3c.jpg, "An overhead light with a rustic look.", "26 x 30 x 31 inches", 5)
-INSERT INTO Products ("Good Wood", "lighting", 119.00, "Glass/Metal", light4a.jpg, light4b.jpg, light4c.jpg, "A simple overhead light.", "27 x 32 x 31.5 inches", 5)
-INSERT INTO Products ("Custom Crafts", "lighting", 239.99, "Glass/Metal", light5a.jpg, light5b.jpg, light5c.jpg, "Upward facing, six hanging overhead.", "25.5 x 30 x 30.5 inches", 5)
+VALUES ("Tri-Bright","White Glow", "lighting", 229.00, "Glass/Metal", light1a.jpg, light1b.jpg, light1c.jpg, "Triple the beauty!", "25 x 29.5 x 31 inches", 5),
+VALUES ("Brooklyn","Custom Crafts", "lighting", 199.99, "Glass/Metal", light2a.jpg, light2b.jpg, light2c.jpg, "Six overhead lights.", "25.5 x 30 x 30.5 inches", 5),
+VALUES ("Rustic Bar","Encono-ture", "lighting", 179.00, "Glass/Metal", light3a.jpg, light3b.jpg, light3c.jpg, "An overhead light with a rustic look.", "26 x 30 x 31 inches", 5),
+VALUES ("Paris","Good Wood", "lighting", 119.00, "Glass/Metal", light4a.jpg, light4b.jpg, light4c.jpg, "A simple overhead light.", "27 x 32 x 31.5 inches", 5),
+VALUES ("Royal","Custom Crafts", "lighting", 239.99, "Glass/Metal", light5a.jpg, light5b.jpg, light5c.jpg, "Upward facing, six hanging overhead.", "25.5 x 30 x 30.5 inches", 5),
 
-INSERT INTO Products ("Good Wood", "mirror", 329.00, "Spruce", mirror1a.jpg, mirror1b.jpg, mirror1c.jpg, "This full length mirror is hand crafted and adjusts to the angle you need.", "25 x 29.5 x 31 inches", 5)
-INSERT INTO Products ("Custom Crafts", "mirror", 79.99, "Pine", mirror2a.jpg, mirror2b.jpg, mirror2c.jpg, "A simple bathroom mirror.", "25.5 x 30 x 30.5 inches", 5)
-INSERT INTO Products ("Encono-ture", "mirror", 39.00, "Metal", mirror3a.jpg, mirror3b.jpg, mirror3c.jpg, "This mirror has two lighted sides, true reflection and magnified, to fit all your needs.", "26 x 30 x 31 inches", 5)
+VALUES ("Vintage Snow White","Good Wood", "mirror", 329.00, "Spruce", mirror1a.jpg, mirror1b.jpg, mirror1c.jpg, "This full length mirror is hand crafted and adjusts to the angle you need.", "25 x 29.5 x 31 inches", 5),
+VALUES ("Jersey","Custom Crafts", "mirror", 79.99, "Pine", mirror2a.jpg, mirror2b.jpg, mirror2c.jpg, "A simple bathroom mirror.", "25.5 x 30 x 30.5 inches", 5),
+VALUES ("Vanity","Encono-ture", "mirror", 39.00, "Metal", mirror3a.jpg, mirror3b.jpg, mirror3c.jpg, "This mirror has two lighted sides, true reflection and magnified, to fit all your needs.", "26 x 30 x 31 inches", 5),
 
-INSERT INTO Products ("White Glow", "table", 59.00, "Pine/Metal", table1a.jpg, table1b.jpg, table1c.jpg, "A simple end table.", "25 x 29.5 x 31 inches", 5)
-INSERT INTO Products ("Custom Crafts", "table", 299.99, "Pine/Metal", table2a.jpg, table2b.jpg, table2c.jpg, "Rollable with locking wheels and center trash cutaway.", "25.5 x 30 x 30.5 inches", 5)
-INSERT INTO Products ("Encono-ture", "table", 49.00, "Pine/Leather", table3a.jpg, table3b.jpg, table3c.jpg, "A small end table that can be used for storage or as a chair.", "26 x 30 x 31 inches", 5)
-INSERT INTO Products ("Good Wood", "table", 219.00, "Pine", table4a.jpg, table4b.jpg, table4c.jpg, "A table useful for storage. It can be extened for more usable surface area.", "27 x 32 x 31.5 inches", 5)
-INSERT INTO Products ("Custom Crafts", "table", 59.99, "Oak", table5a.jpg, table5b.jpg, table5c.jpg, "A classic display table. Perfect for displaying family photos or orchids.", "25.5 x 30 x 30.5 inches", 5)
-INSERT INTO Products ("Custom Crafts", "table", 239.99, "Oak", table6a.jpg, table6b.jpg, table6c.jpg, "Hand crafted with 3 removable leafs to suit your family's needs.", "25.5 x 30 x 30.5 inches", 5)
+VALUES ("Couch Buddy","Encono-ture", "table", 59.00, "Pine/Metal", table1a.jpg, table1b.jpg, table1c.jpg, "A simple end table.", "25 x 29.5 x 31 inches", 5),
+VALUES ("Cutting Rollaway","Custom Crafts", "table", 299.99, "Pine/Metal", table2a.jpg, table2b.jpg, table2c.jpg, "Rollable with locking wheels and center trash cutaway.", "25.5 x 30 x 30.5 inches", 5),
+VALUES ("Encono-ture", "table", 49.00, "Pine/Leather", table3a.jpg, table3b.jpg, table3c.jpg, "A small end table that can be used for storage or as a chair.", "26 x 30 x 31 inches", 5),
+VALUES ("Mini Ottoman","Good Wood", "table", 219.00, "Pine", table4a.jpg, table4b.jpg, table4c.jpg, "A table useful for storage. It can be extened for more usable surface area.", "27 x 32 x 31.5 inches", 5),
+VALUES ("Secret Woods","Custom Crafts", "table", 59.99, "Oak", table5a.jpg, table5b.jpg, table5c.jpg, "A classic display table. Perfect for displaying family photos or orchids.", "25.5 x 30 x 30.5 inches", 5),
+VALUES ("Classic Round","Custom Crafts", "table", 239.99, "Oak", table6a.jpg, table6b.jpg, table6c.jpg, "Hand crafted with 3 removable leafs to suit your family's needs.", "25.5 x 30 x 30.5 inches", 5);
 
 
 -- -----------------------------------------------------
