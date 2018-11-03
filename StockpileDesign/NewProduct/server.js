2dd0260fc7f5122e7659363e64ad9cec1c19804c
Alex <akennedy4155@gmail.com>
2018-11-03 02:48:56 -0700

    beautify and made small change to server to get css to work

--- StockpileDesign/NewProduct/server.js
+++ StockpileDesign/NewProduct/server.js
@@ -8,16 +8,18 @@ var expressValidator = require('express-validator');
 app.use(expressValidator());
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 var methodOverride = require('method-override');
-app.use(methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        var method = req.body._method;
-        delete req.body._method;
-        return method
-    }
+app.use(methodOverride(function(req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method
+  }
 }));
 
 var flash = require('express-flash');
@@ -25,21 +27,24 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 app.use(cookieParser('csci3308'));
 app.use(session({
-    secret: 'csci3308',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {maxAge: 60000}
+  secret: 'csci3308',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 60000
+  }
 }));
 app.use(flash());
 
 //routes
-var index = require('./routes/product_list');
+var chair_list = require('./routes/product_routes');
 //var store = require('./routes/store');
-app.use('/', index);
+app.use('/', chair_list);
+app.use(express.static('./'));
 //app.use('/store', store);
 
 //start server on port 4000
 var port = 4000;
-app.listen(port, function () {
-    console.log('Server running on http://localhost:' + port)
+app.listen(port, function() {
+  console.log('Server running on http://localhost:' + port)
 });
