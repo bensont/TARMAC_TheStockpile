6158ff7bb00c4a9650465a1759153f31628c40fd
Alex <akennedy4155@gmail.com>
2018-11-03 01:56:08 -0700

    weird thing happened with merge conflict

--- StockpileDesign/NavBar/loadDefault.js
+++ StockpileDesign/NavBar/loadDefault.js
@@ -1,11 +1,3 @@
 $(document).ready(function(){
-<<<<<<< HEAD
   $('.stockpile-navbar').load("/StockpileDesign/NavBar/navbar-raw.html");
-=======
-<<<<<<< HEAD
-  $('.stockpile-navbar').load("navbar-raw.html");
-=======
-  $('.stockpile-navbar').load("/StockpileDesign/NavBar/navbar-raw.html");
->>>>>>> navbar-updates
->>>>>>> master
 });
