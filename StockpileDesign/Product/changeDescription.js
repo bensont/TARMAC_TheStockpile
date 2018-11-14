e1a1dc8d13a58a1d5e6593d8fe62131080cbe0fd
Alex-Kennedy-Laptop <akennedy4155@gmail.com>
2018-11-14 16:05:39 -0700

    rm all of the old product stuff

--- StockpileDesign/Product/changeDescription.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function notEmpty() {
-  var e = document.getElementById("legsSelect");
-  var f = document.getElementById("coverSelect");
-  var strUser = f.options[f.selectedIndex].value + ", " + e.options[e.selectedIndex].value;
-  document.getElementById('product-details').innerHTML = strUser;
-  document.getElementById("legsSelect").onchange = notEmpty;
-  document.getElementById("coverSelect").onchange = notEmpty;
-})
