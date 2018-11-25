d4f76e77498d8fa77c51c3ac1d492206bcfbc42c
bensont <36897397+bensont@users.noreply.github.com>
2018-11-25 16:14:28 -0700

    Fixed / bug in material

--- StockpileDesign/Database/databasePostgres.sql
+++ StockpileDesign/Database/databasePostgres.sql
@@ -92,29 +92,29 @@ VALUES ('admin','password','admin','54 Red Street','New York City','New York', 1
 
 INSERT INTO Products (Name, Brand, Type, Cost, Material, Image, Image2, Image3, Description, Dimensions, Stock)
 VALUES ('Classic','Good Wood', 'Chair', 169.00, 'Oak', 'chair2a.jpg', 'chair2b.jpg', 'chair2c.jpg', 'A beautifully crafted chair.', '25 x 29.5 x 31 inches', 5),
-('Classy Classic','Custom Crafts', 'Chair', 199.99, 'Oak/Metal', 'chair3a.jpg''', 'chair3b.jpg', 'chair3c.jpg', 'A chair superior to any bar stool.', '25.5 x 30 x 30.5 inches', 5),
+('Classy Classic','Custom Crafts', 'Chair', 199.99, 'Oak,Metal', 'chair3a.jpg''', 'chair3b.jpg', 'chair3c.jpg', 'A chair superior to any bar stool.', '25.5 x 30 x 30.5 inches', 5),
 ('Royal Bar','Encono-ture', 'Chair', 69.00, 'Pine', 'chair1a.jpg', 'chair1b.jpg', 'chair1c.jpg', 'A basic, but functional chiar', '26 x 30 x 31 inches', 5),
-('Cushoned Presidential','Good Wood', 'Chair', 249.00, 'Oak/Metal', 'chair4a.jpg', 'chair4b.jpg', 'chair4c.jpg', 'A chair that puts comfort in your behind.', '27 x 32 x 31.5 inches', 5),
+('Cushoned Presidential','Good Wood', 'Chair', 249.00, 'Oak,Metal', 'chair4a.jpg', 'chair4b.jpg', 'chair4c.jpg', 'A chair that puts comfort in your behind.', '27 x 32 x 31.5 inches', 5),
 ('Porch Rocker','Custom Crafts', 'Chair', 289.00, 'Oak', 'chair5a.jpg', 'chair5b.jpg', 'chair5c.jpg', 'A classic rocking chair. Quality.', '27.5 x 31 x 31.5 inches', 5),
 
 ('Mirrored Princess','Good Wood', 'Dresser', 369.00, 'Oak', 'dresser1a.jpg', 'dresser1b.jpg', 'dresser1c.jpg', 'A beautifully crafted dresser. Mirror included.', '25 x 29.5 x 31 inches', 5),
 ('Vintage','Custom Crafts', 'Dresser', 299.99, 'Maple', 'dresser2a.jpg', 'dresser2b.jpg', 'dresser2c.jpg', 'Classic, beautifully designed with uniquely patterned wood.', '25.5 x 30 x 30.5 inches', 5),
 ('London Classic','Encono-ture', 'Dresser', 469.00, 'Oak', 'dresser3a.jpg', 'dresser3b.jpg', 'dresser3c.jpg', 'A large dresser with lots of space.', '26 x 30 x 31 inches', 5),
-('London Bedside','Good Wood', 'Dresser', 249.00, 'Oak/Metal', 'dresser4a.jpg', 'dresser4b.jpg', 'dresser4c.jpg', 'A smaller dresser. Very useful as a nightstand.', '27 x 32 x 31.5 inches', 5),
+('London Bedside','Good Wood', 'Dresser', 249.00, 'Oak,Metal', 'dresser4a.jpg', 'dresser4b.jpg', 'dresser4c.jpg', 'A smaller dresser. Very useful as a nightstand.', '27 x 32 x 31.5 inches', 5),
 
-('Tri-Bright','White Glow', 'Lighting', 229.00, 'Glass/Metal', 'light1a.jpg', 'light1b.jpg', 'light1c.jpg', 'Triple the beauty!', '25 x 29.5 x 31 inches', 5),
-('Brooklyn','Custom Crafts', 'Lighting', 199.99, 'Glass/Metal', 'light2a.jpg', 'light2b.jpg', 'light2c.jpg', 'Six overhead lights.', '25.5 x 30 x 30.5 inches', 5),
-('Rustic Bar','Encono-ture', 'Lighting', 179.00, 'Glass/Metal', 'light3a.jpg', 'light3b.jpg', 'light3c.jpg', 'An overhead light with a rustic look.', '26 x 30 x 31 inches', 5),
-('Paris','Good Wood', 'Lighting', 119.00, 'Glass/Metal', 'light4a.jpg', 'light4b.jpg', 'light4c.jpg', 'A simple overhead light.', '27 x 32 x 31.5 inches', 5),
-('Royal','Custom Crafts', 'Lighting', 239.99, 'Glass/Metal', 'light5a.jpg', 'light5b.jpg', 'light5c.jpg', 'Upward facing, six hanging overhead.', '25.5 x 30 x 30.5 inches', 5),
+('Tri-Bright','White Glow', 'Lighting', 229.00, 'Glass,Metal', 'light1a.jpg', 'light1b.jpg', 'light1c.jpg', 'Triple the beauty!', '25 x 29.5 x 31 inches', 5),
+('Brooklyn','Custom Crafts', 'Lighting', 199.99, 'Glass,Metal', 'light2a.jpg', 'light2b.jpg', 'light2c.jpg', 'Six overhead lights.', '25.5 x 30 x 30.5 inches', 5),
+('Rustic Bar','Encono-ture', 'Lighting', 179.00, 'Glass,Metal', 'light3a.jpg', 'light3b.jpg', 'light3c.jpg', 'An overhead light with a rustic look.', '26 x 30 x 31 inches', 5),
+('Paris','Good Wood', 'Lighting', 119.00, 'Glass,Metal', 'light4a.jpg', 'light4b.jpg', 'light4c.jpg', 'A simple overhead light.', '27 x 32 x 31.5 inches', 5),
+('Royal','Custom Crafts', 'Lighting', 239.99, 'Glass,Metal', 'light5a.jpg', 'light5b.jpg', 'light5c.jpg', 'Upward facing, six hanging overhead.', '25.5 x 30 x 30.5 inches', 5),
 
 ('Vintage Snow White','Good Wood', 'Mirror', 329.00, 'Spruce', 'mirror1a.jpg', 'mirror1b.jpg', 'mirror1c.jpg', 'This full length mirror is hand crafted and adjusts to the angle you need.', '25 x 29.5 x 31 inches', 5),
 ('Jersey','Custom Crafts', 'Mirror', 79.99, 'Pine', 'mirror2a.jpg', 'mirror2b.jpg', 'mirror2c.jpg', 'A simple bathroom mirror.', '25.5 x 30 x 30.5 inches', 5),
 ('Vanity','Encono-ture', 'Mirror', 39.00, 'Metal', 'mirror3a.jpg', 'mirror3b.jpg', 'mirror3c.jpg', 'This mirror has two lighted sides, true reflection and magnified, to fit all your needs.', '26 x 30 x 31 inches', 5),
 
-('Couch Buddy','Encono-ture', 'Table', 59.00, 'Pine/Metal', 'table1a.jpg', 'table1b.jpg', 'table1c.jpg', 'A simple end table.', '25 x 29.5 x 31 inches', 5),
-('Cutting Rollaway','Custom Crafts', 'Table', 299.99, 'Pine/Metal', 'table2a.jpg', 'table2b.jpg', 'table2c.jpg', 'Rollable with locking wheels and center trash cutaway.', '25.5 x 30 x 30.5 inches', 5),
-('Mini Ottoman','Encono-ture', 'Table', 49.00, 'Pine/Leather', 'table3a.jpg', 'table3b.jpg', 'table3c.jpg', 'A small end table that can be used for storage or as a chair.', '26 x 30 x 31 inches', 5),
+('Couch Buddy','Encono-ture', 'Table', 59.00, 'Pine,Metal', 'table1a.jpg', 'table1b.jpg', 'table1c.jpg', 'A simple end table.', '25 x 29.5 x 31 inches', 5),
+('Cutting Rollaway','Custom Crafts', 'Table', 299.99, 'Pine,Metal', 'table2a.jpg', 'table2b.jpg', 'table2c.jpg', 'Rollable with locking wheels and center trash cutaway.', '25.5 x 30 x 30.5 inches', 5),
+('Mini Ottoman','Encono-ture', 'Table', 49.00, 'Pine,Leather', 'table3a.jpg', 'table3b.jpg', 'table3c.jpg', 'A small end table that can be used for storage or as a chair.', '26 x 30 x 31 inches', 5),
 ('Secret Woods','Good Wood', 'Table', 219.00, 'Pine', 'table4a.jpg', 'table4b.jpg', 'table4c.jpg', 'A table useful for storage. It can be extened for more usable surface area.', '27 x 32 x 31.5 inches', 5),
 ('The Dumont','Custom Crafts', 'Table', 59.99, 'Oak', 'table5a.jpg', 'table5b.jpg', 'table5c.jpg', 'A classic display table. Perfect for displaying family photos or orchids.', '25.5 x 30 x 30.5 inches', 5),
 ('Classic Round','Custom Crafts', 'Table', 239.99, 'Oak', 'table6a.jpg', 'table6b.jpg', 'table6c.jpg', 'Hand crafted with 3 removable leafs to suit the needs of your family.', '25.5 x 30 x 30.5 inches', 5);
